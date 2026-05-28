f5eee158db7d125f44773e931173399c1293562b
adndrsd1 <adindarizki15@gmail.com>
2026-05-28 22:28:25 +0700

    fix run path(2)

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,7 +6,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-# Hubungkan path file dataset secara aman menggunakan relative path dinamis
 current_dir = os.path.dirname(os.path.abspath(__file__))
 df = pd.read_csv(os.path.join(current_dir, "../Titanic-Dataset_preprocessing.csv"))
 
@@ -20,26 +19,16 @@ X_train, X_test, y_train, y_test = train_test_split(
     random_state=42
 )
 
-# HAPUS BARIS: mlflow.set_tracking_uri(...) 
-# HAPUS BARIS: mlflow.set_experiment(...)
-# Biarkan parameter tracking diatur secara otomatis oleh file manifes MLproject
-
-# Aktifkan perekaman metrik otomatis dari scikit-learn
 mlflow.sklearn.autolog(log_models=True)
 
-# ========================================================
-# PROSES TRAINING & EVALUASI
-# ========================================================
 model = RandomForestClassifier(
     n_estimators=100,
     max_depth=5,
     random_state=42
 )
 
-# Tarik proses pelatihan (Metrik akurasi otomatis direkam oleh autolog)
 model.fit(X_train, y_train)
 
-# Prediksi komponen test
 preds = model.predict(X_test)
 acc = accuracy_score(y_test, preds)
 
