eb5f2f4f5d77c80929ad6ae8067b28d97d2b8353
adndrsd1 <adindarizki15@gmail.com>
2026-05-28 21:32:44 +0700

    update and fixing modelling

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,45 +1,26 @@
 import pandas as pd
 import mlflow
 import mlflow.sklearn
-
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-# AUTLOG
-mlflow.sklearn.autolog()
+mlflow.set_tracking_uri("file:./mlruns")
+mlflow.set_experiment("CI Training")
+mlflow.sklearn.autolog(log_models=True)
 
-# Load dataset
 df = pd.read_csv("../Titanic-Dataset_preprocessing.csv")
-
 X = df.drop("Survived", axis=1)
 y = df["Survived"]
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X,
-    y,
-    test_size=0.2,
-    random_state=42
-)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Model
-model = RandomForestClassifier(
-    n_estimators=100,
-    max_depth=5,
-    random_state=42
-)
+with mlflow.start_run():
+    model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=42)
+    model.fit(X_train, y_train)
+    preds = model.predict(X_test)
+    acc = accuracy_score(y_test, preds)
+    mlflow.log_metric("accuracy", acc)
+    print("Accuracy:", acc)
 
-# Training
-model.fit(X_train, y_train)
-
-# Prediction
-preds = model.predict(X_test)
-
-# Accuracy
-acc = accuracy_score(y_test, preds)
-
-# Log metric tambahan
-mlflow.log_metric("accuracy", acc)
-
-print("Accuracy:", acc)
-print("Training CI selesai")
+print("Training selesai")
