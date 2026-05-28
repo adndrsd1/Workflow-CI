8e84fc3f43e89b5df9366e0ca44d0da190e449fe
adndrsd1 <adindarizki15@gmail.com>
2026-05-28 22:05:31 +0700

    chore: fulfill criteria 3 using native mlflow run with nested context

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,22 +1,42 @@
 import pandas as pd
 import mlflow
 import mlflow.sklearn
+import os
+
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-mlflow.set_tracking_uri("file:./mlruns")
-mlflow.set_experiment("CI Training")
-mlflow.sklearn.autolog(log_models=True)
+current_dir = os.path.dirname(os.path.abspath(__file__))
+mlruns_path = os.path.join(current_dir, "mlruns")
+mlflow.set_tracking_uri(f"file:{mlruns_path}")
 
-df = pd.read_csv("../Titanic-Dataset_preprocessing.csv")
+mlflow.set_experiment("CI Training")
+
+mlflow.sklearn.autolog(
+    log_models=True
+)
+
+df = pd.read_csv(os.path.join(current_dir, "../Titanic-Dataset_preprocessing.csv"))
+
 X = df.drop("Survived", axis=1)
 y = df["Survived"]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
 
-with mlflow.start_run():
-    model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=42)
+with mlflow.start_run(nested=True):
+
+    model = RandomForestClassifier(
+        n_estimators=100,
+        max_depth=5,
+        random_state=42
+    )
+
     model.fit(X_train, y_train)
     preds = model.predict(X_test)
     acc = accuracy_score(y_test, preds)
